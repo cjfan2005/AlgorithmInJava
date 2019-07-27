e7e8909515cd5a22f2300cf55ac9cea62bfa7603
Kent <kentfan@localhost>
2019-07-27 11:22:35 +0800

    GCD Recursive with Euclidean algorithm. 輾轉相除法，又稱歐幾里得算法

--- src/kent/alg/recursive/App.java
+++ src/kent/alg/recursive/App.java
@@ -4,10 +4,14 @@ public class App {
 
 	public static void main(String[] args) {
 		
-		IterativeAndRecursive a = new IterativeAndRecursive();
-		System.out.println("Iterative result = " + a.sumIterative(3));
+//		IterativeAndRecursive a = new IterativeAndRecursive();
+//		System.out.println("Iterative result = " + a.sumIterative(3));
+//		System.out.println("Recursive result = " + a.sumRecursive(5));
 		
-		System.out.println("Recursive result = " + a.sumRecursive(5));
+		
+		Euclidean e = new Euclidean();
+		System.out.println(e.gcdIteractive(12, 4));
+		System.out.println(e.gcdRecursive(12, 4));
 		
 	}
 }
