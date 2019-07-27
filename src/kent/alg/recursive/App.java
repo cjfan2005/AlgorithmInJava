ce2fb918584c9411ae28b6fdd41c453c5c341ea1
Kent <kentfan@localhost>
2019-07-27 12:16:42 +0800

    Linear Search

--- src/kent/alg/recursive/App.java
+++ src/kent/alg/recursive/App.java
@@ -9,9 +9,13 @@ public class App {
 //		System.out.println("Recursive result = " + a.sumRecursive(5));
 		
 		
-		Euclidean e = new Euclidean();
-		System.out.println(e.gcdIteractive(12, 4));
-		System.out.println(e.gcdRecursive(12, 4));
+//		Euclidean e = new Euclidean();
+//		System.out.println(e.gcdIteractive(12, 4));
+//		System.out.println(e.gcdRecursive(12, 4));
+		
+		int[] array = {15,2,3,7,10,5,9,8,14,22};
+		LinearSearch ls = new LinearSearch(array);
+		System.out.println(ls.linearSerch(99));
 		
 	}
 }
---
+++ src/kent/alg/recursive/LinearSearch.java
@@ -0,0 +1,24 @@
+package kent.alg.recursive;
+
+
+/*
+ * Time Complexity: 
+ * 
+ * (1+2+3+...+n)/n = (n+1)/2  ->  Ο(n)
+ */
+public class LinearSearch {
+
+	int[] array;
+	
+	public LinearSearch(int[] array) {
+		this.array = array;
+	}
+	
+	public int linearSerch(int item) {
+		for(int i=0; i<array.length; i++) 
+			if(array[i] == item)
+				return i;
+		
+		return -1;
+	}
+}
