18569c3e81de697e4aae6918cada95a3686d928f
KentFan <cjfan2005@gmail.com>
2019-08-13 18:06:49 +0800

    add two numbers

---
+++ src/kent/alg/leetcode/AddTwoNumbers.java
@@ -0,0 +1,34 @@
+package kent.alg.leetcode;
+/*
+ You are given two non-empty linked lists representing two non-negative integers. 
+ The digits are stored in reverse order and each of their nodes contain a single digit. 
+ Add the two numbers and return it as a linked list.
+ You may assume the two numbers do not contain any leading zero, except the number 0 itself.
+ 
+ Input: (2 -> 4 -> 3) + (5 -> 6 -> 4)
+ Output: 7 -> 0 -> 8
+ Explanation: 342 + 465 = 807.
+ 
+ */
+public class AddTwoNumbers {
+	class ListNode{
+	    int val;
+	    ListNode nextNode;
+	    ListNode(int val){
+	        this.val=val;
+	        this.nextNode=null;
+	    }
+	}
+	
+	public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
+		
+		ListNode dummy = new ListNode(0);
+		
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
