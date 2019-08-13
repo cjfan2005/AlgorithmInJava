558a7c47335386e00a5cf424cc273944475b0e0d
KentFan <cjfan2005@gmail.com>
2019-08-13 17:30:56 +0800

    add test main function

--- src/kent/alg/leetcode/TwoSum.java
+++ src/kent/alg/leetcode/TwoSum.java
@@ -37,6 +37,8 @@ public class TwoSum {
 	}
 	
 	public static void main(String[] args) {
-		
+		int[] nums = {2, 7, 11, 15};
+		int target = 18;
+        System.out.println(twoSum(nums, target));
 	}
 }
