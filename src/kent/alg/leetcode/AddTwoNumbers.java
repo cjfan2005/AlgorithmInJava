2c82591ae6d6c7e7eb9362ede2d4ea56f34e1bd0
Kent <kentfan@192.168.0.108>
2019-08-13 22:54:57 +0800

    finish add two numbers

--- src/kent/alg/leetcode/AddTwoNumbers.java
+++ src/kent/alg/leetcode/AddTwoNumbers.java
@@ -23,7 +23,29 @@ public class AddTwoNumbers {
 	public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
 		
 		ListNode dummy = new ListNode(0);
+		int sum = 0;
+		ListNode cur = dummy;
+		ListNode p1 = l1, p2 = l2;
 		
+		while(p1 != null && p2 != null) {
+			if(p1 != null) {
+				sum += p1.val;
+				p1 = p1.nextNode;
+			}
+			if(p2 != null) {
+				sum += p2.val;
+				p1 = p2.nextNode;
+			}
+			cur.nextNode = new ListNode(sum%10);
+			sum /= 10;
+			cur = cur.nextNode;
+		}
+		
+		if(sum == 1) {
+			cur.nextNode = new ListNode(1);
+			
+		}
+		return dummy.nextNode;
 	}
 
 	public static void main(String[] args) {
