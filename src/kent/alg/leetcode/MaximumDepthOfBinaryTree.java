9312365f88ef1c736d5209eb96a2e9f985a5d8a9
KentFan <cjfan2005@gmail.com>
2019-08-14 19:01:39 +0800

    MaximumDepthOfBinaryTree

--- src/kent/alg/leetcode/MaximumDepthOfBinaryTree.java
+++ src/kent/alg/leetcode/MaximumDepthOfBinaryTree.java
@@ -1,5 +1,13 @@
 package kent.alg.leetcode;
+/**
+Given a binary tree, find its maximum depth.
 
+The maximum depth is the number of nodes along the longest path from the root node down to the farthest leaf node.
+
+Note: A leaf is a node with no children.
+ * @author kentfan
+ *
+ */
 public class MaximumDepthOfBinaryTree {
 
 	/**
@@ -11,7 +19,12 @@ public class MaximumDepthOfBinaryTree {
 	    TreeNode(int x) { val = x; }
 	}
 	
-	
+	public int maxDepth(TreeNode root) {
+        if (root == null) return 0;
+        
+        int depest = Math.max(maxDepth(root.left), maxDepth(root.right)) + 1;
+        return depest;
+    }
 	 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
