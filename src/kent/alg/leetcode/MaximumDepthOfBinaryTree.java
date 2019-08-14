c95a40c7d588d96038d1afef4f229d83d91c490b
KentFan <cjfan2005@gmail.com>
2019-08-14 19:01:09 +0800

    MaximumDepthOfBinaryTree

---
+++ src/kent/alg/leetcode/MaximumDepthOfBinaryTree.java
@@ -0,0 +1,21 @@
+package kent.alg.leetcode;
+
+public class MaximumDepthOfBinaryTree {
+
+	/**
+	 * Definition for a binary tree node. */
+	public class TreeNode {
+	    int val;
+	    TreeNode left;
+	    TreeNode right;
+	    TreeNode(int x) { val = x; }
+	}
+	
+	
+	 
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
