9508349680d6a8ee349f7b52e00a8808740e2725
KentFan <cjfan2005@gmail.com>
2019-08-14 16:41:59 +0800

    MaximumSubarray

---
+++ src/kent/alg/leetcode/MaximumSubarray.java
@@ -0,0 +1,33 @@
+package kent.alg.leetcode;
+/**
+Given an integer array nums, find the contiguous subarray 
+(containing at least one number) which has the largest sum and return its sum.
+
+Example:
+	Input: [-2,1,-3,4,-1,2,1,-5,4],
+	Output: 6
+	Explanation: [4,-1,2,1] has the largest sum = 6.
+
+Follow up:
+  If you have figured out the O(n) solution, 
+  try coding another solution using the divide and conquer approach, which is more subtle.
+
+ 
+ */
+public class MaximumSubarray {
+	
+	public int maxSubArray(int[] nums) {
+		int[] dp = new int[nums.length];
+		
+		dp[0] = nums[0];
+		int res = nums[0];
+		
+		for(int i=1; i<nums.length; i++) {
+			dp[i] = nums[i] + (dp[i-1] < 0 ? 0 : dp[i-1]);
+			res = Math.max(res, dp[i]);
+		}
+		return res;
+	}
+	
+
+}
